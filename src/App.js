fa4ded6ec340179c5bd6400e84cd38cd7c4d3379
muktaalkunte <muktaalkunte@gmail.com>
2023-08-31 11:07:03 +0530

    Unknow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Axios from "axios";
+import { useState } from "react";
+import "./App.css";
+import RecipeTile from "./RecipeTile";
 
 function App() {
+  const [query, setquery] = useState("");
+  const [recipes, setrecipes] = useState([]);
+  const[healthLabels,sethealthLabels]=useState("vegan");
+
+  const YOUR_APP_ID=`26236842`;
+ const YOUR_APP_KEY=`3901e93dc8ab42a8d3a7942c0a4defe4`;
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabels}`;
+
+  const getRecipeInfo = async () => {
+    var result = await Axios.get(url);
+    setrecipes(result.data.hits);
+    console.log(result.data.hits);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipeInfo();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 onClick={getRecipeInfo}> Recipe Hunt 🍔</h1>
+      <form className="app__searchForm" onSubmit={onSubmit}>
+        <input
+          className="app__input"
+          type="text"
+          placeholder="enter ingredient"
+          autoComplete="Off"
+          value={query}
+          onChange={(e) => setquery(e.target.value)}
+        />
+        <input className="app__submit" type="submit" value="Search" />
+
+        <select className="apphealthLabels">
+        <option onClick={()=>sethealthLabels("vegan")}>Vegan</option>
+        <option onClick={()=>sethealthLabels("vegetarian")}>Vegetarian</option>
+      </select>
+      </form>
+
+      <div className="app__recipes">
+        {recipes !== [] &&
+          recipes.map((recipe) => {
+            return <RecipeTile recipe={recipe} />;
+          })}
+      </div>
     </div>
   );
 }
---
+++ src/RecipeTile.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import "./RecipeTile.css";
+import { v4 as uuidv4 } from "uuid";
+
+export default function RecipeTile({ recipe }) {
+  return (
+    
+      <div
+        className="recipeTile"
+        onClick={() => window.open(recipe["recipe"]["url"])}
+      >
+        <img className="recipeTile__img" src={recipe["recipe"]["image"]} />
+        <p className="recipeTile__name" key={uuidv4()}>
+          {recipe["recipe"]["label"]}
+        </p>
+      </div>
+    
+  );
+}
+
